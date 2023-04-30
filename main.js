ab278a02a73512ad02f482407e8b1d172516e130
Elvinho <elvison_victor004@hotmail.comm>
2023-04-30 13:54:40 -0300

    adicionando pagina login

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain,Notification } = require("electron");
 const path = require("path");
 
 const createWindow = () => {
@@ -21,25 +21,46 @@ const createWindow = () => {
   // mainWindow.webContents.openDevTools()
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Algumas APIs podem ser usadas somente depois que este evento ocorre.
-app.whenReady().then(() => {
-  createWindow();
+function loginWindow(){
+  winlogin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload:path.join(__dirname,"login.js")
+    }
+  })
+  winlogin.loadFile("login.html");
+}
 
-  app.on("activate", () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+app.whenReady().then(loginWindow)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. Você também pode colocar eles em arquivos separados e requeridos-as aqui.
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+ipcMain.handle("login",(event,obj)=>{
+  validateLogin(obj)
+})
+
+function validateLogin(obj){
+  const {email, password} = obj;
+
+    if(email ==='test' && password === "123"){
+      createWindow()
+      window.show();
+      winlogin().close()
+    }
+    else{
+      new Notification({
+        title:"login",
+        body:"Erro!"
+      }).show()
+    }
+
+}
